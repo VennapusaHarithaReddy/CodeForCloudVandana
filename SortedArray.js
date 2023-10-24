f9dc0154065057c8c2452bf39dd06b7cfd363713
VennapusaHarithaReddy <148863491+VennapusaHarithaReddy@users.noreply.github.com>
2023-10-24 17:35:40 +0530

    Create SortedArray.js

---
+++ SortedArray.js
@@ -0,0 +1,27 @@
+
+B.
+function bubbleSortDescending(arr) {
+    let n = arr.length;
+    let swapped;
+    
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] < arr[i + 1]) {
+               
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        n--; 
+    } while (swapped);
+
+    return arr;
+}
+
+
+const unsortedArray = [5, 2, 9, 1, 5, 6];
+const sortedArray = bubbleSortDescending(unsortedArray);
+console.log("Sorted array in descending order:", sortedArray);
