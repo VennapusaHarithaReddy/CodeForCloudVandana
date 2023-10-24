c864b26812e57db78f49b960f8e550c46fd1d961
VennapusaHarithaReddy <148863491+VennapusaHarithaReddy@users.noreply.github.com>
2023-10-24 17:39:08 +0530

    RomanToInteger.java

---
+++ RomanToInteger.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String romanNumeral = "IX"; 
+
+        int result = romanToInt(romanNumeral);
+        System.out.println("Integer representation of " + romanNumeral + " is: " + result);
+    }
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int currentVal = romanMap.get(s.charAt(i));
+            
+            if (i < s.length() - 1) {
+                int nextVal = romanMap.get(s.charAt(i + 1));
+                if (currentVal < nextVal) {
+                    result -= currentVal;
+                } else {
+                    result += currentVal;
+                }
+            } else {
+                result += currentVal;
+            }
+        }
+
+        return result;
+    }
+}
