6e236c5632b30f7dc88c0bc19f7268ea431fc9dc
VennapusaHarithaReddy <148863491+VennapusaHarithaReddy@users.noreply.github.com>
2023-10-24 17:35:12 +0530

    Update and rename JavaScript to ReverseWords.js

--- ReverseWords.js
+++ ReverseWords.js
@@ -24,27 +24,6 @@ const inputSentence = "This is a sunny day";
 const reversedSentence = reverseWordsInSentence(inputSentence);
 console.log(reversedSentence); 
 
-B.
-function bubbleSortDescending(arr) {
-    let n = arr.length;
-    let swapped;
-    
-    do {
-        swapped = false;
-        for (let i = 0; i < n - 1; i++) {
-            if (arr[i] < arr[i + 1]) {
-               
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
-            }
-        }
-        n--; 
-    } while (swapped);
-
-    return arr;
-}
 
 
 const unsortedArray = [5, 2, 9, 1, 5, 6];
